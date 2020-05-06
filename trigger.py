7b63b452996065bc05b38044d1116590a00c77dc
Raimon Esteve <raimonesteve@gmail.com>
2020-05-06 16:02:26 +0200

    Simplify trigger action issue9096

--- trigger.py
+++ trigger.py
@@ -1,7 +1,6 @@
 # This file is part electronic_mail_template module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-"Trigger Extension"
 from trytond.model import fields
 from trytond.transaction import Transaction
 from trytond.pool import Pool, PoolMeta
@@ -11,8 +10,14 @@ __all__ = ['Trigger']
 
 class Trigger(metaclass=PoolMeta):
     __name__ = 'ir.trigger'
-
     email_template = fields.Many2One('electronic.mail.template', "Template")
+
+    @classmethod
+    def __setup__(cls):
+        super().__setup__()
+        cls.action.selection.append(
+            ('electronic.mail.template|mail_from_trigger', "Email Template"),
+            )
 
     @staticmethod
     def default_model():
