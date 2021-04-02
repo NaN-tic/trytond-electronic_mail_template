c2383a0ff823d47325135e22809291bc1f19b0e0
Bernat Brunet <bernat@nan-tic.com>
2021-04-02 19:41:04 +0200

    Add get attachments functions from records. used in electrenic_mail_singel module

--- template.py
+++ template.py
@@ -7,7 +7,7 @@
 import logging
 import mimetypes
 from email import encoders, charset
-from email.header import decode_header, Header
+from email.header import Header
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
@@ -336,6 +336,29 @@ class Template(ModelSQL, ModelView):
         Trigger = Pool().get('ir.trigger')
         trigger = Trigger(trigger_id)
         return cls.render_and_send(trigger.email_template.id, records)
+
+    def get_attachments(self, records):
+        record_ids = [r.id for r in records]
+        attachments = []
+        for report in self.reports:
+            report = Pool().get(report.report_name, type='report')
+            ext, data, filename, file_name = report.execute(record_ids, {})
+
+            if file_name:
+                filename = self.eval(file_name, record_ids).decode('utf-8')
+            filename = ext and '%s.%s' % (filename, ext) or filename
+            content_type, _ = mimetypes.guess_type(filename)
+            maintype, subtype = (
+                content_type or 'application/octet-stream'
+                ).split('/', 1)
+
+            attachment = MIMEBase(maintype, subtype)
+            attachment.set_payload(data)
+            encoders.encode_base64(attachment)
+            attachment.add_header(
+                'Content-Disposition', 'attachment', filename=filename)
+            attachments.append(attachment)
+        return attachments
 
 
 class TemplateReport(ModelSQL):
