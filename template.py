8e2bdab3d6c015da44a4bfcb0bc85670a0604c4f
Albert Cervera i Areny <albert@nan-tic.com>
2024-01-03 16:58:37 +0100

    Use a specific policy for sending e-mails as the default one is not valid for SMTP | #164124
    More precisely, by default Python does not use CRLF which is required by
    the SMTP standard.
    
    Many servers including GMail, AWS SES and probably older MS Exchange
    accept CR-only but new Office 365 seems that it no longer accepts them.

--- template.py
+++ template.py
@@ -9,6 +9,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email.utils import formatdate, make_msgid
+from email import policy
 from genshi.template import TextTemplate
 try:
     from jinja2 import Template as Jinja2Template
@@ -160,6 +161,11 @@ class Template(ModelSQL, ModelView):
         template_context = cls.template_context(record)
         return template.render(template_context)
 
+    @staticmethod
+    def _get_policy():
+        # See https://docs.python.org/3/library/email.policy.html
+        return policy.compat32.clone(linesep='\r\n', raise_on_defect=True)
+
     @classmethod
     def render(cls, template, record, values):
         '''Renders the template and returns as email object
@@ -175,7 +181,8 @@ class Template(ModelSQL, ModelView):
         # Remember to use unix2dos before uploading for check as smtplib does
         # that conversion automatically.
         ElectronicMail = Pool().get('electronic.mail')
-        message = MIMEMultipart()
+
+        message = MIMEMultipart(policy=cls._get_policy())
         messageid = template.eval(values['message_id'], record)
         message['Message-Id'] = messageid or make_msgid()
         message['Date'] = formatdate(localtime=1)
@@ -229,18 +236,22 @@ class Template(ModelSQL, ModelView):
             html = "%s%s" % (html, footer)
         body = None
         if html and plain:
-            body = MIMEMultipart('alternative')
-        charset.add_charset('utf-8')
+            body = MIMEMultipart('alternative', policy=cls._get_policy())
+        charset.add_charset('utf-8', charset.QP, charset.QP)
         if plain:
             if body:
-                body.attach(MIMEText(plain, 'plain', _charset='utf-8'))
+                body.attach(MIMEText(plain, 'plain', _charset='utf-8',
+                        policy=cls._get_policy()))
             else:
-                message.attach(MIMEText(plain, 'plain', _charset='utf-8'))
+                message.attach(MIMEText(plain, 'plain', _charset='utf-8',
+                        policy=cls._get_policy()))
         if html:
             if body:
-                body.attach(MIMEText(html, 'html', _charset='utf-8'))
+                body.attach(MIMEText(html, 'html', _charset='utf-8',
+                        policy=cls._get_policy()))
             else:
-                message.attach(MIMEText(html, 'html', _charset='utf-8'))
+                message.attach(MIMEText(html, 'html', _charset='utf-8',
+                        policy=cls._get_policy()))
         if body:
             message.attach(body)
 
@@ -258,7 +269,8 @@ class Template(ModelSQL, ModelView):
                     content_type or 'application/octet-stream'
                     ).split('/', 1)
 
-                attachment = MIMEBase(maintype, subtype)
+                attachment = MIMEBase(maintype, subtype,
+                    policy=cls._get_policy())
                 attachment.set_payload(data)
                 encoders.encode_base64(attachment)
                 attachment.add_header(
@@ -377,7 +389,7 @@ class Template(ModelSQL, ModelView):
                 content_type or 'application/octet-stream'
                 ).split('/', 1)
 
-            attachment = MIMEBase(maintype, subtype)
+            attachment = MIMEBase(maintype, subtype, policy=self._get_policy())
             attachment.set_payload(data)
             encoders.encode_base64(attachment)
             attachment.add_header(
